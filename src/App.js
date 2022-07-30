48411683d39f9f5e3d997a0e36c21c7b03177155
Stefanie <stefanie@workwithhuddle.com>
2022-07-30 18:06:59 +0100

    feat: created initial list of pokemon with pagination

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
+
 import './App.css';
+import {useState, useEffect } from 'react';
+import axios from 'axios';
+import PokemonList from './components/PokemonList/PokemonList';
+import Pagination from './components/Pagination/Pagination';
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState('https://pokeapi.co/api/v2/pokemon/')
+  const [nextPageUrl, setNextPageUrl] = useState()
+  const [prevPageUrl, setPrevPageUrl] = useState()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setLoading(true)
+    let cancel
+    axios.get(currentPageUrl,  {
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    }).then(res => {
+      setLoading(false)
+      setNextPageUrl(res.data.next)
+      setPrevPageUrl(res.data.previous)
+      setPokemon(res.data.results.map(pokemon => pokemon))
+    })
+
+    return () => cancel()
+  }, [currentPageUrl])
+
+  function goToNextPage() {
+    setCurrentPageUrl(nextPageUrl)
+  }
+
+  function goToPrevPage() {
+    setCurrentPageUrl(prevPageUrl)
+  }
+
+  if (loading) return "Loading..."
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello</h1>
-      </header>
+      <PokemonList  pokemon={pokemon}/>
+      <Pagination 
+        goToNextPage={nextPageUrl ? goToNextPage : null}
+        goToPrevPage={prevPageUrl ? goToPrevPage : null}
+      />
+
     </div>
   );
 }
